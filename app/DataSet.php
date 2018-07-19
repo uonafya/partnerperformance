18e0ce847e7e3971e58fdba0bbfaaefaf748028f
Joel <joelkith@gmail.com>
2018-07-19 21:08:30 +0300

    DHIS

--- app/DataSet.php
+++ app/DataSet.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class DataSet extends Model
 {
-    //
+
+	public function element()
+	{
+		return $this->hasMany('App\DataSetElement');
+	}
 }
--- app/DataSetElement.php
+++ app/DataSetElement.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class DataSetElement extends Model
 {
-    //
+
+	public function dataset()
+	{
+		return $this->belongsTo('App\DataSet');
+	}
 }
