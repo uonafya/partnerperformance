1fa1da8d5256abc2e3c40b7f86976c916fddd84a
Joel <joelkith@gmail.com>
2018-08-05 13:09:47 +0300

    Home Page

--- resources/views/base/home.blade.php
+++ resources/views/base/home.blade.php
@@ -56,19 +56,6 @@
 </div>
 
 <div class="row">
-	<div class="col-md-12 col-sm-12 col-xs-12">
-		<div class="panel panel-default">
-		    <div class="panel-heading">
-			    New on Treatment Trend <div class="display_date"></div>
-		    </div>
-			<div class="panel-body" id="newtx">
-				<center><div class="loader"></div></center>
-			</div>
-		</div>
-	</div>
-</div>
-
-<div class="row">
 	<div class="col-md-6 col-sm-12 col-xs-12">
 		<div class="panel panel-default">
 		    <div class="panel-heading">
