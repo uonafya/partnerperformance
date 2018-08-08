86fb2159225bf8942afe5116d1ed0d88518c1dd8
Joel <joelkith@gmail.com>
2018-08-08 10:01:59 +0300

    Fix summing queries

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -116,8 +116,8 @@ class Controller extends BaseController
     public function old_gender_query()
     {
     	return "
-    		(SUM(`male_under_15yrs_receiving_hiv_pos_results`) + SUM(`male_15-24yrs_receiving_hiv_pos_results` + `male_above_25yrs_receiving_hiv_pos_results`)) AS male_test,
-    		(SUM(`female_under_15yrs_receiving_hiv_pos_results`) + SUM(`female_15-24yrs_receiving_hiv_pos_results` + `female_above_25yrs_receiving_hiv_pos_results`)) AS female_test
+    		(SUM(`male_under_15yrs_receiving_hiv_pos_results`) + SUM(`male_15-24yrs_receiving_hiv_pos_results`) + SUM(`male_above_25yrs_receiving_hiv_pos_results`)) AS male_test,
+    		(SUM(`female_under_15yrs_receiving_hiv_pos_results`) + SUM(`female_15-24yrs_receiving_hiv_pos_results`) + SUM(`female_above_25yrs_receiving_hiv_pos_results`)) AS female_test
 		";
     }
 
