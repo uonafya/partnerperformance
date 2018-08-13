e988a2f284808fcf0533e2370fed8bd398048aed
Joel <joelkith@gmail.com>
2018-08-14 00:16:11 +0300

    stuff

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,9 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-    	\App\User::create(['name' => 'joel', 'email' => 'joelkith@gmail.com', 'password' => 'password', 'partner_id' => 55, 'user_type_id' => 1])
+    	\App\User::create(['name' => 'joel', 'email' => 'joelkith@gmail.com', 'password' => 'password', 'partner_id' => 55, 'user_type_id' => 1]);
+    	\App\User::create(['name' => 'tim', 'email' => 'tngugi@gmail.com', 'password' => 'password', 'partner_id' => 55, 'user_type_id' => 1]);
+    	\App\User::create(['name' => 'joshua', 'email' => 'baksajoshua09@gmail.com', 'password' => 'password', 'partner_id' => 55, 'user_type_id' => 1]);
+    	\App\User::create(['name' => 'James Batuka', 'email' => 'jbatuka@usaid.gov', 'password' => 'password', 'partner_id' => 55, 'user_type_id' => 1]);
     }
 }
