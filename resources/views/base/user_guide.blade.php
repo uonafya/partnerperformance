a611558ad30efea2790ab22b1777bbb16268462f
Joel <joelkith@gmail.com>
2018-08-13 07:56:39 +0300

    stuff

---
+++ resources/views/base/user_guide.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.master')
+
+@section('content')
+
+<style type="text/css">
+	.display_date {
+		width: 130px;
+		display: inline;
+	}
+	.display_date {
+		width: 130px;
+		display: inline;
+	}
+</style>
+
+
+<div class="row">
+	<div class="col-md-12 col-sm-12 col-xs-12">
+		<div class="panel panel-default">
+		    <div class="panel-heading">
+			    User Guide 
+		    </div>
+			<div class="panel-body" id="user_guide">
+				<p>
+					This dashboard pull 
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+@endsection
+
+
+@section('scripts')
+
+@endsection
+
+
