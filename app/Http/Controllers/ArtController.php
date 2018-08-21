91f73f75e0ca92e5fac50d3f69a8cf2cf1e5f5de
Joel <joelkith@gmail.com>
2018-08-22 01:13:00 +0300

    Changes

--- app/Http/Controllers/ArtController.php
+++ app/Http/Controllers/ArtController.php
@@ -131,72 +131,6 @@ class ArtController extends Controller
 		return view('charts.bar_graph', $data);
 	}
 
-	public function new_age_breakdown()
-	{
-		$date_query = Lookup::date_query();
-		$divisions_query = Lookup::divisions_query();
-
-		$rows = DB::table('d_hiv_and_tb_treatment')
-			->join('view_facilitys', 'view_facilitys.id', '=', 'd_hiv_and_tb_treatment.facility')
-			->selectRaw($this->new_art_query())
-			->addSelect('year', 'month')
-			->whereRaw($date_query)
-			->whereRaw($divisions_query)
-			->groupBy('year', 'month')
-			->orderBy('year', 'asc')
-			->orderBy('month', 'asc')
-			->get();
-
-		$rows2 = DB::table('d_care_and_treatment')
-			->join('view_facilitys', 'view_facilitys.id', '=', 'd_care_and_treatment.facility')
-			->selectRaw($this->former_new_art_query())
-			->addSelect('year', 'month')
-			->whereRaw($date_query)
-			->whereRaw($divisions_query)
-			->groupBy('year', 'month')
-			->orderBy('year', 'asc')
-			->orderBy('month', 'asc')
-			->get();
-
-		$date_query = Lookup::date_query(true);
-		$target = DB::table('t_hiv_and_tb_treatment')
-			->join('view_facilitys', 'view_facilitys.id', '=', 't_hiv_and_tb_treatment.facility')
-			->selectRaw("SUM(`start_art_total_(sum_hv03-018_to_hv03-029)_hv03-026`) AS `total`")
-			->whereRaw($date_query)
-			->whereRaw($divisions_query)
-			->first();
-
-		$t = round(($target->total / 12), 2);
-
-		$data['div'] = str_random(15);
-
-		$data['outcomes'][0]['name'] = "Below 1";
-		$data['outcomes'][1]['name'] = "Below 15";
-		$data['outcomes'][2]['name'] = "Above 15";
-		$data['outcomes'][3]['name'] = "Monthly Target";
-
-		$data['outcomes'][0]['type'] = "column";
-		$data['outcomes'][1]['type'] = "column";
-		$data['outcomes'][2]['type'] = "column";
-		$data['outcomes'][3]['type'] = "spline";
-
-		$data['outcomes'][0]['tooltip'] = array("valueSuffix" => ' ');
-		$data['outcomes'][1]['tooltip'] = array("valueSuffix" => ' ');
-		$data['outcomes'][2]['tooltip'] = array("valueSuffix" => ' ');
-		$data['outcomes'][3]['tooltip'] = array("valueSuffix" => ' ');
-
-		foreach ($rows as $key => $row) {
-			$data['categories'][$key] = Lookup::get_category($row->year, $row->month);
-			$data["outcomes"][0]["data"][$key] = (int) $row->below_1 + $rows2[$key]->below_1;
-			$data["outcomes"][1]["data"][$key] = (int) $row->below_10 + $row->below_15 + $rows2[$key]->below_15;
-			$data["outcomes"][2]["data"][$key] = (int) $row->below_20 + $row->below_25 + $row->above_25 + $rows2[$key]->above_15;
-			$data["outcomes"][3]["data"][$key] = $t;
-		}
-		return view('charts.bar_graph', $data);		
-	}
-
-
-
 	public function current_age_breakdown()
 	{
 		$date_query = Lookup::date_query();
@@ -216,6 +150,65 @@ class ArtController extends Controller
 		$rows2 = DB::table('d_care_and_treatment')
 			->join('view_facilitys', 'view_facilitys.id', '=', 'd_care_and_treatment.facility')
 			->selectRaw($this->former_age_current_query())
+			->addSelect('year', 'month')
+			->whereRaw($date_query)
+			->whereRaw($divisions_query)
+			->groupBy('year', 'month')
+			->orderBy('year', 'asc')
+			->orderBy('month', 'asc')
+			->get();
+
+
+		$date_query = Lookup::date_query(true);
+		$target = DB::table('t_hiv_and_tb_treatment')
+			->join('view_facilitys', 'view_facilitys.id', '=', 't_hiv_and_tb_treatment.facility')
+			->selectRaw("SUM(`on_art_total_(sum_hv03-034_to_hv03-043)_hv03-038`) AS `total`")
+			->whereRaw($date_query)
+			->whereRaw($divisions_query)
+			->first();
+
+		$data['div'] = str_random(15);
+
+		$data['outcomes'][0]['name'] = "Below 1";
+		$data['outcomes'][1]['name'] = "Below 15";
+		$data['outcomes'][2]['name'] = "Above 15";
+		$data['outcomes'][3]['name'] = "Target";
+
+		$data['outcomes'][0]['type'] = "column";
+		$data['outcomes'][1]['type'] = "column";
+		$data['outcomes'][2]['type'] = "column";
+		$data['outcomes'][3]['type'] = "spline";
+
+		foreach ($rows as $key => $row) {
+			$data['categories'][$key] = Lookup::get_category($row->year, $row->month);
+			$data["outcomes"][0]["data"][$key] = (int) $row->below_1 + $rows2[$key]->below_1;
+			$data["outcomes"][1]["data"][$key] = (int) $row->below_10 + $row->below_15 + $rows2[$key]->below_15;
+			$data["outcomes"][2]["data"][$key] = (int) $row->below_20 + $row->below_25 + $row->above_25 + $rows2[$key]->above_15;
+
+			$data["outcomes"][3]["data"][$key] = (int) $target->total;
+		}
+		return view('charts.bar_graph', $data);
+	}
+
+	public function new_age_breakdown()
+	{
+		$date_query = Lookup::date_query();
+		$divisions_query = Lookup::divisions_query();
+
+		$rows = DB::table('d_hiv_and_tb_treatment')
+			->join('view_facilitys', 'view_facilitys.id', '=', 'd_hiv_and_tb_treatment.facility')
+			->selectRaw($this->new_art_query())
+			->addSelect('year', 'month')
+			->whereRaw($date_query)
+			->whereRaw($divisions_query)
+			->groupBy('year', 'month')
+			->orderBy('year', 'asc')
+			->orderBy('month', 'asc')
+			->get();
+
+		$rows2 = DB::table('d_care_and_treatment')
+			->join('view_facilitys', 'view_facilitys.id', '=', 'd_care_and_treatment.facility')
+			->selectRaw($this->former_new_art_query())
 			->addSelect('year', 'month')
 			->whereRaw($date_query)
 			->whereRaw($divisions_query)
@@ -257,14 +250,15 @@ class ArtController extends Controller
 			->get();
 
 
-
 		$date_query = Lookup::date_query(true);
 		$target = DB::table('t_hiv_and_tb_treatment')
 			->join('view_facilitys', 'view_facilitys.id', '=', 't_hiv_and_tb_treatment.facility')
-			->selectRaw("SUM(`on_art_total_(sum_hv03-034_to_hv03-043)_hv03-038`) AS `total`")
+			->selectRaw("SUM(`start_art_total_(sum_hv03-018_to_hv03-029)_hv03-026`) AS `total`")
 			->whereRaw($date_query)
 			->whereRaw($divisions_query)
 			->first();
+
+		$t = round(($target->total / 12), 2);
 
 		$data['div'] = str_random(15);
 
@@ -272,7 +266,7 @@ class ArtController extends Controller
 		$data['outcomes'][1]['name'] = "Below 15";
 		$data['outcomes'][2]['name'] = "Above 15";
 		$data['outcomes'][3]['name'] = "Positive Tests";
-		$data['outcomes'][4]['name'] = "Target";
+		$data['outcomes'][4]['name'] = "Monthly Target";
 
 		$data['outcomes'][0]['type'] = "column";
 		$data['outcomes'][1]['type'] = "column";
@@ -292,11 +286,12 @@ class ArtController extends Controller
 			$data["outcomes"][2]["data"][$key] = (int) $row->below_20 + $row->below_25 + $row->above_25 + $rows2[$key]->above_15;
 
 			$data["outcomes"][3]["data"][$key] = (int) $rows3[$key]->pos + $rows4[$key]->pos;
-
-			$data["outcomes"][4]["data"][$key] = (int) $target->total;
+			$data["outcomes"][4]["data"][$key] = $t;
 		}
-		return view('charts.bar_graph', $data);
+		return view('charts.bar_graph', $data);		
 	}
+
+
 
 	public function enrolled_age_breakdown()
 	{
