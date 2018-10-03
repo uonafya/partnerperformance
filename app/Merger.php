54b80f1a201992a0e453b44b11b1e22072c45c55
Joel <joelkith@gmail.com>
2018-10-03 11:13:56 +0300

    Merge records from the 2 forms

--- app/Merger.php
+++ app/Merger.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use DB;
+use \App\Synch;
 
 class Merger
 {
@@ -18,6 +19,16 @@ class Merger
 	{
 		self::merge_rows($year, 'merge_testing', 'd_hiv_testing_and_prevention_services', 'd_hiv_counselling_and_testing', 'm_testing');
 	}
+
+    public static function art($year=null)
+    {
+        self::merge_rows($year, 'merge_art', 'd_hiv_and_tb_treatment', 'd_care_and_treatment', 'm_art');
+    }
+
+    public static function pmtct($year=null)
+    {
+        self::merge_rows($year, 'merge_pmtct', 'd_prevention_of_mother-to-child_transmission', 'd_pmtct', 'm_pmtct');
+    }
 
 	public static function merge_rows($year, $function_name, $new_table, $old_table, $merged_table)
 	{
@@ -147,17 +158,48 @@ class Merger
     	return $data;
     }
 
-    public static function pmtct($row, $old_row)
+    public static function merge_pmtct($row, $old_row)
     {
-    	// $data['tested_pmtct'] = self::merged_value($row->{'enrolled_<1_hv03-001'}, $old_row->{'total_tested_(pmtct)'});
+        $initial_tests = $row->{'initial_test_at_anc_hv02-04'} + $row->{'initial_test_at_l&d_hv02-05'} + $row->{'initial_test_at_pnc_pnc<=6wks_hv02-06'};
+    	$data['tested_pmtct'] = self::merged_value($initial_tests, $old_row->{'total_tested_(pmtct)'});
 
-    	$known_pos_new = $row->{'known_positive_at_1st_anc_hv02-03'} ?? $row->{'known_positive_at_1st_anc_hv02-10'};
+    	$data['initial_test_anc'] = self::merged_value($row->{'initial_test_at_anc_hv02-04'}, $old_row->{'antenatal_testing_for_hiv'});
+    	$data['initial_test_lnd'] = self::merged_value($row->{'initial_test_at_l&d_hv02-05'}, $old_row->{'labour_and_delivery_testing_for_hiv'});
+    	$data['initial_test_pnc'] = self::merged_value($row->{'initial_test_at_pnc_pnc<=6wks_hv02-06'}, $old_row->{'postnatal_(within_72hrs)_testing_for_hiv'});
 
-    	$data['known_pos_anc'] = self::merged_value($known_pos_new, $old_row->{'known_positive_status_(at_entry_into_anc)'});
+        $known_pos_new = $row->{'known_positive_at_1st_anc_hv02-03'} ?? $row->{'known_positive_at_1st_anc_hv02-10'};
 
-    	$data['initial_test_anc'] = self::merged_value($row->{'initial_test_at_anc_hv02-04'}, $old_row->{'started_on_art_during_anc'});
-    	$data['initial_test_lnd'] = $row->{'initial_test_at_l&d_hv02-05'};
-    	$data['initial_test_pnc'] = $row->{'initial_test_at_pnc_pnc<=6wks_hv02-06'};
+        $data['known_pos_anc'] = self::merged_value($known_pos_new, $old_row->{'known_positive_status_(at_entry_into_anc)'});
+
+        $data['positives_anc'] = self::merged_value($row->{'positive_results_anc_hv02-11'}, $old_row->{'antenatal_positive_to_hiv_test'});
+        $data['positives_lnd'] = self::merged_value($row->{'positive_results_l&d_hv02-12'}, $old_row->{'labour_and_delivery_postive_to_hiv_test'});
+        $data['positives_pnc'] = self::merged_value($row->{'positive_results_pnc<=6wks_hv02-13'}, $old_row->{'postnatal_(within_72hrs)_postive_to_hiv_test'});
+        $data['positives_pnc6m'] = $row->{'positive_pnc>_6weeks_to_6_months_hv02-14'};
+
+        $data['total_positive_pmtct'] = self::merged_value($row->{'total_positive_(add_hv02-10_-_hv02-14)_hv02-15'}, $old_row->{'total_positive_(pmtct)'});
+
+        $data['total_new_positive_pmtct'] = $data['total_positive_pmtct'] - $data['known_pos_anc'];
+
+        $data['haart_total'] = self::merged_value($row->{'on_maternal_haart_total_hv02-20'}, $old_row->{'total_pmtct_prophylaxis'});
+
+        $data['on_haart_anc'] = $row->{'on_haart_at_1st_anc_hv02-16'};
+        $data['start_art_anc'] = self::merged_value($row->{'start_haart_anc_hv02-17'}, $old_row->{'started_on_art_during_anc'});
+        $data['start_art_lnd'] = $row->{'start_haart_l&d_hv02-18'};
+        $data['start_art_pnc'] = $row->{'start_haart_pnc<=6wks_hv02-19'};
+        $data['start_art_pnc6m'] = $row->{'start_haart_pnc>_6weeks_to_6_months_hv02-21'};
+
+        $data['known_status_before_male'] = $row->{'known_status_1st_contact_hv02-29'};
+        $data['initial_male_test_anc'] = self::merged_value($row->{'initial_test_at_anc_male_hv02-30'}, $old_row->{'male_partners_tested_-(_anc/l&d)'});
+        $data['initial_male_test_lnd'] = $row->{'initial_test_at_l&d_male_hv02-31'};
+        $data['initial_male_test_pnc'] = $row->{'initial_test_at_pnc_male_hv02-32'};
+        $data['known_status_male'] = $row->{'total_known_status_male_hv02-33'};
+
+        $data['initial_pcr_2m'] = self::merged_value($row->{'initial_pcr_<_8wks_hv02-44'}, $old_row->{'pcr_(within_2_months)_infant_testing_(initial_test_only)'});
+        $data['initial_pcr_12m'] = self::merged_value($row->{'initial_pcr_>8wks_-12_mths_hv02-45'}, 
+            ($old_row->{'pcr_(from_9_to_12_months)_infant_testing_(initial_test_only)'} + 
+                $old_row->{'pcr_(from3_to_8_months)_infant_testing_(initial_test_only)'}));
+
+        $data['confirmed_pos'] = self::merged_value($row->{'infected_24mths_hv02-47'}, $old_row->{'total_confirmed_positive_infant_test_result_by_pcr'});
     }
 
 
@@ -293,31 +335,67 @@ class Merger
             initial_test_lnd int(10) DEFAULT NULL,
             initial_test_pnc int(10) DEFAULT NULL,
 
-            known_hiv_status int(10) DEFAULT NULL,
-
             positives_anc int(10) DEFAULT NULL,
             positives_lnd int(10) DEFAULT NULL,
             positives_pnc int(10) DEFAULT NULL,
+            positives_pnc6m int(10) DEFAULT NULL,
 
             total_positive_pmtct int(10) DEFAULT NULL,
+            total_new_positive_pmtct int(10) DEFAULT NULL,
+
+            haart_total int(10) DEFAULT NULL,
+
+            on_haart_anc int(10) DEFAULT NULL,
+            start_art_anc int(10) DEFAULT NULL,
+            start_art_lnd int(10) DEFAULT NULL,
+            start_art_pnc int(10) DEFAULT NULL,
+            start_art_pnc_6m int(10) DEFAULT NULL,
+
+            known_status_before_male int(10) DEFAULT NULL,
 
             initial_male_test_anc int(10) DEFAULT NULL,
             initial_male_test_lnd int(10) DEFAULT NULL,
             initial_male_test_pnc int(10) DEFAULT NULL,
 
-            haart_total int(10) DEFAULT NULL,
+            known_status_male int(10) DEFAULT NULL,
 
-            start_art_anc int(10) DEFAULT NULL,
-            start_art_lnd int(10) DEFAULT NULL,
-            start_art_pnc int(10) DEFAULT NULL,
-            start_art_pnc_g6w int(10) DEFAULT NULL,
-
-
-
+            initial_pcr_2m int(10) DEFAULT NULL,
+            initial_pcr_12m int(10) DEFAULT NULL,
+            confirmed_pos int(10) DEFAULT NULL,
 
         ";
 
         self::table_base('m_pmtct'. $pmtct);
+    }
+
+    public static function insert_rows($year=null)
+    {        
+        if(!$year) $year = date('Y');
+        $tables = ['m_testing', 'm_art', 'm_pmtct'];
+
+        foreach ($tables as $table) {
+
+            $i=0;
+            $data_array = [];
+
+            for ($month=1; $month < 13; $month++) { 
+                foreach ($facilities as $k => $val) {
+                    $data = array('year' => $year, 'month' => $month, 'facility' => $val->id);
+                    $data = array_merge($data, Synch::get_financial_year_quarter($year, $month) );
+                    $data_array[$i] = $data;
+                    $i++;
+
+                    if ($i == 200) {
+                        DB::connection('mysql_wr')->table($table)->insert($data_array);
+                        $data_array=null;
+                        $i=0;
+                    }
+                }
+            }
+            if($data_array) DB::connection('mysql_wr')->table($table)->insert($data_array);
+
+            echo  'Completed entry for ' . $table . " \n";
+        }
     }
 
     public static function table_base($table_name, $columns)
