93ff4dca81e6b15bcd65a67e87a0b5f0df0ecf9c
Joel <joelkith@gmail.com>
2018-10-31 13:59:26 +0300

    Updates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,8 +46,8 @@ class UserController extends Controller
         $user->save();
 
         $mail_array = [$user->email];
-        Mail::to($mail_array)->cc(['jbatuka@usaid.gov', 'joelkith@gmail.com'])->send(new NewUser($user));
-        // Mail::to($mail_array)->cc(['joelkith@gmail.com'])->send(new NewUser($user));
+        // Mail::to($mail_array)->cc(['jbatuka@usaid.gov', 'joelkith@gmail.com'])->send(new NewUser($user));
+        Mail::to($mail_array)->cc(['joelkith@gmail.com'])->send(new NewUser($user));
 
         session(['toast_message' => 'User Created.']);
 
