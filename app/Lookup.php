0155e0f252404c5bd21677004b4c2725a0b030be
Joel <joelkith@gmail.com>
2018-11-02 09:13:45 +0300

    Updates

--- app/Lookup.php
+++ app/Lookup.php
@@ -548,7 +548,7 @@ class Lookup
 		if(session('filter_subcounty')) $query .= " AND subcounty_id" . self::set_division_query(session('filter_subcounty'));
 		if(session('filter_ward')) $query .= " AND ward_id" . self::set_division_query(session('filter_ward'));
 		if(session('filter_facility')) $query .= " AND view_facilitys.id" . self::set_division_query(session('filter_facility'));
-		if(session('filter_partner')) $query .= " AND partner" . self::set_division_query(session('filter_partner'));
+		if(session('filter_partner') || is_numeric(session('filter_partner'))) $query .= " AND partner" . self::set_division_query(session('filter_partner'));
 		if(session('filter_agency')) $query .= " AND funding_agency_id" . self::set_division_query(session('filter_agency'));
 
 		return $query;
