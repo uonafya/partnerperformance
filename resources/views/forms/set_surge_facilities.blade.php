b02f4eabf1e99efb0f551a8964ea2db4d4e69623
Joel <joelkith@gmail.com>
2019-04-16 09:38:38 +0300

    Updates

--- resources/views/forms/set_surge_facilities.blade.php
+++ resources/views/forms/set_surge_facilities.blade.php
@@ -27,7 +27,7 @@
 					@csrf
 
           
-          <div class="form-group">
+          {{--<div class="form-group">
             <label class="col-sm-4 control-label">Surge Facilities</label>
             <div class="col-sm-8">
               @foreach($facilities as $facility)
@@ -38,11 +38,27 @@
                         checked='true'
                       @endif
                     />
-                    {{ $facility->facilitycode . ' ' . $facility->name }}
+                    {{ $facility->facilitycode . ' - ' . $facility->name }}
                   </label>
                 </div>
               @endforeach    
             </div>        
+          </div>--}}
+
+          <div class="form-group">
+            <label class="col-sm-3 control-label">Surge Facilities (Select Multiple)</label>
+            <div class="col-sm-9">
+              <select class="select_tag" name="facilities[]" multiple="multiple" required>
+                <option></option>
+                @foreach($facilities as $facility)
+                  <option value="{{ $facility->id }}"
+                      @if($facility->is_surge)
+                        selected='true'
+                      @endif
+                    > {{ $facility->facilitycode . ' - ' . $facility->name }} </option>
+                @endforeach
+              </select>
+            </div>
           </div>
 
 
