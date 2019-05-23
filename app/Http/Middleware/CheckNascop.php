c2af6611dc436e9a7ce7cdb82636d2a8b3c1ae48
Joel <joelkith@gmail.com>
2019-05-23 13:32:55 +0300

    Updates

--- app/Http/Middleware/CheckNascop.php
+++ app/Http/Middleware/CheckNascop.php
@@ -18,14 +18,14 @@ class CheckNascop
     public function handle($request, Closure $next)
     {
         if(env('DB_HOST') == '10.231.111.110'){
-            // $base = 'https://kemrinairobi.nascop.org:7443/api/';
-            // $client = new Client(['base_uri' => $base]); 
-            // try {
-            //     $response = $client->request('get', 'hello_nascop', ['http_errors' => false, 'timeout' => 1]);
-            //     if($response->getStatusCode() == 200) abort(500, 'NASCOP is back online.<br />Please clear your cache then try again.');
-            // } catch (Exception $e) {
+            $base = 'https://kemrinairobi.nascop.org:7443/api/';
+            $client = new Client(['base_uri' => $base]); 
+            try {
+                $response = $client->request('get', 'hello_nascop', ['http_errors' => false, 'timeout' => 1]);
+                if($response->getStatusCode() == 200) abort(500, 'NASCOP is back online.<br />Please clear your cache then try again.');
+            } catch (Exception $e) {
                 
-            // }
+            }
         }
         
         return $next($request);
