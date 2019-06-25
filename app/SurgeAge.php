f0c498058cfb00b8932ca9a69464ee8cb63d66bf
Joel <joelkith@gmail.com>
2019-06-26 00:21:01 +0300

    Migrated to using periods table

--- app/SurgeAge.php
+++ app/SurgeAge.php
@@ -9,21 +9,22 @@ class SurgeAge extends BaseModel
 
     public function scopeSurge($query)
     {
-        return $query->where(['for_surge' => 1])->orderBy('age_category_id', 'asc')->orderBy('id', 'asc');
+        // return $query->where(['for_surge' => 1])->orderBy('age_category_id', 'asc')->orderBy('id', 'asc');
+        return $query->where(['for_surge' => 1])->orderBy('max_age', 'asc');
     }
 	
     public function scopeVmmc($query)
     {
-        return $query->where(['for_vmmc' => 1])->orderBy('age_category_id', 'asc')->orderBy('id', 'asc');
+        return $query->where(['for_vmmc' => 1])->orderBy('max_age', 'asc');
     }
 	
     public function scopeTx($query)
     {
-        return $query->where(['for_tx_curr' => 1])->orderBy('age_category_id', 'asc')->orderBy('id', 'asc');
+        return $query->where(['for_tx_curr' => 1])->orderBy('max_age', 'asc');
     }
     
     public function scopePrep($query)
     {
-        return $query->where('age_category_id', '!=', 2)->orderBy('age_category_id', 'asc')->orderBy('id', 'asc');
+        return $query->where('age_category_id', '!=', 2)->orderBy('max_age', 'asc');
     }
 }
