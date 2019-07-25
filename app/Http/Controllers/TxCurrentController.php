b906bf3eab292285ad93c57a8a5b797c95230810
Joel <joelkith@gmail.com>
2019-07-25 16:45:17 +0300

    New Reports Analysis

--- app/Http/Controllers/TxCurrentController.php
+++ app/Http/Controllers/TxCurrentController.php
@@ -60,7 +60,7 @@ class TxCurrentController extends Controller
 			$needle = Lookup::get_category($row);
 			$key = array_search($needle, $data['categories']);
 
-			if(!$key) $data['categories'][] = $needle;
+			if($key == false) $data['categories'][] = $needle;
 			$key = array_search($needle, $data['categories']);
 
 			if($row->gender == 'male') $item = 0;
