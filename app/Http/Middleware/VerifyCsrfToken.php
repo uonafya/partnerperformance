6ffb6a602cf1809ff40ec04d2a635ab3240ab4cc
Joel <joelkith@gmail.com>
2019-09-10 12:56:27 +0300

    Updates

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends BaseVerifier
      */
     protected $except = [
         'filter/date',
-        'filter/any'
+        'filter/any',
+        'facility/search',
     ];
 }
