2a3d1aa711a1de2f05b4f60dc061083179382703
Joel <joelkith@gmail.com>
2019-10-06 22:08:52 +0300

    Updates

--- routes/console.php
+++ routes/console.php
@@ -62,7 +62,6 @@ Artisan::command('surges', function () {
 Artisan::command('surges:export', function () {
 	\App\Surge::surge_export();
 })->describe('Surges export.');
-})->describe('Surges.');
 
 Artisan::command('insert:weekly', function () {
 	\App\Other::create_weeks();
