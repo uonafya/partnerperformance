cd422f699b04212ee82d026b3fab324e6bdf366b
Joel <joelkith@gmail.com>
2019-10-09 20:19:44 +0300

    Charts Tests

---
+++ tests/Feature/ChartsTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ChartsTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    /*public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }*/
+
+    public function testCharts()
+    {
+        $routes = [
+            'testing' => ['testing_outcomes', 'testing_age', 'testing_gender', 'pos_age', 'pos_gender', 'positivity', 'discordancy', 'testing_summary', 'summary'],
+
+            'pmtct' => ['haart', 'testing', 'starting_point', 'discovery_positivity', 'eid', 'male_testing'],
+
+            'art' => ['current_age_breakdown', 'new_age_breakdown', 'enrolled_age_breakdown', 'new_art', 'current_art', 'current_suppression', 'treatment', 'reporting'],
+
+            'vmmc' => ['testing', 'summary', 'adverse'],
+
+            'tb' => ['known_status', 'newly_tested', 'tb_screening', 'ipt'],
+
+            'keypop' => ['testing', 'summary', 'current_tx'],
+
+            'non_mer', ['facilities_count', 'clinics', 'achievement', 'breakdown', 'clinics_setup', 'otz_breakdown', 'dsd_impact', 'mens_impact'],
+
+            'regimen' => ['reporting', 'summary'],
+
+            'indicators' => ['testing', 'positivity', 'summary', 'currenttx', 'newtx'],
+
+            'pns' => ['summary_chart', 'pns_contribution', 'summary_table', 'get_table/screened'],
+
+            'surge' => ['testing', 'linkage', 'modality_yield', 'gender_yield', 'age_yield', 'pns', 'tx_sv', 'tx_btc', 'targets'],
+
+        ];
+
+        foreach ($routes as $base => $ends) {
+            foreach ($ends as $end) {
+                $response = $this->get('/' . $base . '/' . $end);
+                $response->assertStatus(200);
+            }
+        }
+    }
+}
