cca541b87695753a8cce837f3119a669b3bc766f
Joel <joelkith@gmail.com>
2020-05-13 18:11:27 +0300

    GBV

--- app/Imports/GbvImport.php
+++ app/Imports/GbvImport.php
@@ -35,7 +35,7 @@ class GbvImport implements OnEachRow, WithHeadingRow
 
     public function onRow(Row $row)
     {
-    	dd($row->gbv_sexual_violence_unknown_male);
+    	dd($row->toArray()['gbv_sexual_violence_unknown_male']);
     	$row = json_decode(json_encode($row->toArray()));
     	// dd($row);
     	if(!is_numeric($row->mfl_code) || (is_numeric($row->mfl_code) && $row->mfl_code < 10000)) return;
