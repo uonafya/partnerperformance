71d34f3a132d909e8a90983abc3e9b56dcd748b5
Joel <joelkith@gmail.com>
2020-11-30 23:32:36 +0300

    Updates

--- app/Imports/GBVImport.php
+++ app/Imports/GBVImport.php
@@ -44,7 +44,7 @@ class GBVImport implements OnEachRow, WithHeadingRow
 		if(!$fac) return;
 
 		$period = Period::where(['financial_year' => $row->financial_year, 'month' => $row->month])->first();
-		if(auth()->user()->id == 1) $period = Period::where(['year' => $row->calendar_year, 'month' => $row->month])->first();
+		// if(auth()->user()->id == 1) $period = Period::where(['year' => $row->calendar_year, 'month' => $row->month])->first();
 		if(!$period) return;
 
 		$update_data = ['dateupdated' => date("Y-m-d")];
