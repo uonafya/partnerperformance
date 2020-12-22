44ef3357f6c016e7e997884842171f82a9b89ebf
Joel <joelkith@gmail.com>
2020-12-22 10:39:17 +0300

    Updates

--- app/Surge.php
+++ app/Surge.php
@@ -558,19 +558,19 @@ class Surge
         $tx_new = SurgeColumnView::where(['modality' => 'tx_new'])->get();
         $pmtct = SurgeColumnView::whereIn('modality', ['pmtct_anc1', 'pmtct_post_anc'])->get();
 
-        $sql = "countyname as County, Subcounty, facilitycode AS `MFL Code`, partnername AS Partner, name AS `Facility`, financial_year AS `Financial Year`, " . $this->get_sum($tested, 'HTS_Tested') . ', ' . $this->get_sum($pos, 'HTS_Positive') . ', ' . $this->get_sum($tx_new, 'tx_new') . ', ' . $this->get_sum($pmtct, 'pmtct');
-        // $this->sql = "facility, financial_year AS `Financial Year`, " . $this->get_sum($tested, 'HTS_Tested') . ', ' . $this->get_sum($pos, 'HTS_Positive') . ', ' . $this->get_sum($tx_new, 'tx_new') . ', ' . $this->get_sum($pmtct, 'pmtct');
+        // $sql = "countyname as County, Subcounty, facilitycode AS `MFL Code`, partnername AS Partner, name AS `Facility`, financial_year AS `Financial Year`, " . $this->get_sum($tested, 'HTS_Tested') . ', ' . $this->get_sum($pos, 'HTS_Positive') . ', ' . $this->get_sum($tx_new, 'tx_new') . ', ' . $this->get_sum($pmtct, 'pmtct');
+        $this->sql = "facility, financial_year AS `Financial Year`, " . $this->get_sum($tested, 'HTS_Tested');
 
         $rows = DB::table('d_surge')
-            ->join('view_facilitys', 'view_facilitys.id', '=', 'd_surge.facility')
+            // ->join('view_facilitys', 'view_facilitys.id', '=', 'd_surge.facility')
             ->join('weeks', 'weeks.id', '=', 'd_surge.week_id')
             ->selectRaw($sql)
             // ->where(['financial_year' => 2020, ])
             ->where(['financial_year' => 2020, 'funding_agency_id' => 1, ])
             // ->whereRaw(Lookup::get_active_partner_query('2020-01-01'))
             ->groupBy('d_surge.facility')
-            ->orderBy('name', 'asc')
-            // ->orderBy('d_surge.facility', 'asc')
+            // ->orderBy('name', 'asc')
+            ->orderBy('d_surge.facility', 'asc')
             ->get();
         dd($rows);
     }
