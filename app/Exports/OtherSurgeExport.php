84856b862d9426012b820c9b85123cb6d0b69fcd
Joel <joelkith@gmail.com>
2020-12-22 10:01:47 +0300

    Updates

--- app/Exports/OtherSurgeExport.php
+++ app/Exports/OtherSurgeExport.php
@@ -38,35 +38,36 @@ class OtherSurgeExport extends BaseExport
 		$tx_new = SurgeColumnView::where(['modality' => 'tx_new'])->get();
 		$pmtct = SurgeColumnView::whereIn('modality', ['pmtct_anc1', 'pmtct_post_anc'])->get();
 
-		// $this->sql = "countyname as County, Subcounty, facilitycode AS `MFL Code`, partnername AS Partner, name AS `Facility`, financial_year AS `Financial Year`, week_number as `Week Number`, " . $this->get_sum($hts, 'hts') . ', ' . $this->get_sum($tx_new, 'tx_new') . ', ' . $this->get_sum($pmtct, 'pmtct');
+		// $this->sql = "countyname as County, Subcounty, facilitycode AS `MFL Code`, partnername AS Partner, name AS `Facility`, financial_year AS `Financial Year`, " . $this->get_sum($tested, 'HTS_Tested') . ', ' . $this->get_sum($pos, 'HTS_Positive') . ', ' . $this->get_sum($tx_new, 'tx_new') . ', ' . $this->get_sum($pmtct, 'pmtct');
 		$this->sql = "countyname as County, Subcounty, facilitycode AS `MFL Code`, partnername AS Partner, name AS `Facility`, financial_year AS `Financial Year`, " . $this->get_sum($tested, 'HTS_Tested') . ', ' . $this->get_sum($pos, 'HTS_Positive') . ', ' . $this->get_sum($tx_new, 'tx_new') . ', ' . $this->get_sum($pmtct, 'pmtct');
 
     }
 
     public function headings() : array
     {
-		$row = DB::table('d_surge')
+		/*$row = DB::table('d_surge')
 			->join('view_facilities', 'view_facilities.id', '=', 'd_surge.facility')
 			->join('weeks', 'weeks.id', '=', 'd_surge.week_id')
 			->selectRaw($this->sql)
-			->where(['week_id' => 0])
+			->where(['week_number' => 5])
 			->whereRaw(Lookup::get_active_partner_query('2020-01-01'))
 			->first();
 
-		return collect($row)->keys()->all();
+		return collect($row)->keys()->all();*/
+
+		return ['County', 'Subcounty', 'MFL Code', 'Partner', 'Facility', 'Financial Year', 'HTS Tested', 'HTS Positive', 'TX NEW', 'PMTCT']
     }
 
 
     public function query()
     {
-		dd(DB::table('d_surge')
-					->join('view_facilities', 'view_facilities.id', '=', 'd_surge.facility')
-					->join('weeks', 'weeks.id', '=', 'd_surge.week_id')
-					->selectRaw($this->sql)
-					->where(['financial_year' => 2020, 'funding_agency_id' => 1, ])
-					->whereRaw(Lookup::get_active_partner_query('2020-01-01'))
-					->groupBy('d_surge.facility')
-					->orderBy('name', 'asc')
-					->get());
+		return DB::table('d_surge')
+			->join('view_facilities', 'view_facilities.id', '=', 'd_surge.facility')
+			->join('weeks', 'weeks.id', '=', 'd_surge.week_id')
+			->selectRaw($this->sql)
+			->where(['financial_year' => 2020, 'funding_agency_id' => 1, ])
+			->whereRaw(Lookup::get_active_partner_query('2020-01-01'))
+			->groupBy('d_surge.facility')
+			->orderBy('name', 'asc');
     }
 }
