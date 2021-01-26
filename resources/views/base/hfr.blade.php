b33b705a3ea3cfa43f143195ca75f65ecf900475
Your Name <joelkith@gmail.com>
2021-01-26 17:35:28 +0300

    Updates

--- resources/views/base/hfr.blade.php
+++ resources/views/base/hfr.blade.php
@@ -58,7 +58,17 @@
 </div>
 
 <div class="row">
-	<div class="col-md-12 col-sm-12 col-xs-12">
+	<div class="col-md-3 col-sm-12 col-xs-12">
+		<div class="panel panel-default">
+		    <div class="panel-heading">
+			    TX CURR Target <div class="display_date"></div>
+		    </div>
+			<div class="panel-body" id="target_donut_tx_curr">
+				<center><div class="loader"></div></center>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-9 col-sm-12 col-xs-12">
 		<div class="panel panel-default">
 		    <div class="panel-heading">
 			    TX CURR <div class="display_date"></div>
