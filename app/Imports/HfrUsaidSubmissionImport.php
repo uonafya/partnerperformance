1c973df74453cec16449e404c35da290239b50fb
Your Name <joelkith@gmail.com>
2021-02-11 17:36:22 +0300

    Updates

--- app/Imports/HfrUsaidSubmissionImport.php
+++ app/Imports/HfrUsaidSubmissionImport.php
@@ -48,7 +48,7 @@ class HfrUsaidSubmissionImport implements OnEachRow, WithHeadingRow, WithChunkRe
 		if(!$fac) dd("facility " . $row->orgunit . ' uid ' . $row->orgunituid . ' not found');
 
 		$date_format = 'm/d/Y';
-		$date_format = 'm/d/y';
+		// $date_format = 'm/d/y';
 
 		$week = Week::where(['start_date' => Carbon::createFromFormat($date_format, $row->date)->toDateString()])->first();
 		if(!$week) dd("Week starting on " . $row->date . " facility " . $row->orgunit . ' uid ' . $row->orgunituid . ' not found');
