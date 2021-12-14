69ff57c79194f98fed620b0d2b2ea31999ec06d2
Benzer <benzerbett@gmail.com>
2021-12-14 15:27:25 +0300

    Download PDF WiP

--- resources/views/base/hfr.blade.php
+++ resources/views/base/hfr.blade.php
@@ -355,29 +355,42 @@
 <script type="text/javascript">
     function downloadPDF() {
 		$(".detail_tables").removeClass("hidden");
-		var HTML_Width = $(".content-body").width();
-		var HTML_Height = $(".content-body").height();
-		var top_left_margin = 15;
-		var PDF_Width = HTML_Width + (top_left_margin * 2);
-		var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
-		var canvas_image_width = HTML_Width;
-		var canvas_image_height = HTML_Height;
+		// var HTML_Width = $(".content-body").width();
+		// var HTML_Height = $(".content-body").height();
+		// var top_left_margin = 15;
+		// var PDF_Width = HTML_Width + (top_left_margin * 2);
+		// var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+		// var canvas_image_width = HTML_Width;
+		// var canvas_image_height = HTML_Height;
 
-		var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+		// var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
 
-		html2canvas($(".content-body")[0]).then(function (canvas) {
-			var imgData = canvas.toDataURL("image/jpeg", 1.0);
-			var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
-			pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
-			for (var i = 1; i <= totalPDFPages; i++) { 
-				pdf.addPage(PDF_Width, PDF_Height);
-				pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height*i)+(top_left_margin*4),canvas_image_width,canvas_image_height);
-			}
-			pdf.save("hfr.pdf");
-		}).then(function(){
-			$(".detail_tables").addClass("hidden");
-		});
+		// html2canvas($(".content-body")[0]).then(function (canvas) {
+		// 	var imgData = canvas.toDataURL("image/jpeg", 1.0);
+		// 	var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+		// 	pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
+		// 	for (var i = 1; i <= totalPDFPages; i++) { 
+		// 		pdf.addPage(PDF_Width, PDF_Height);
+		// 		pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height*i)+(top_left_margin*4),canvas_image_width,canvas_image_height);
+		// 	}
+		// 	pdf.save("hfr.pdf");
+		// }).then(function(){
+		// 	$(".detail_tables").addClass("hidden");
+		// });
 		
+
+		///////
+		var element = document.getElementById('#body');
+		var opt = {
+			margin: 1,
+			filename:     'HFR-'+new Date().toLocaleString('en-GB').replaceAll('/', '_').replaceAll(' ', '_').toLowerCase().replaceAll(',','_').replaceAll(':','_')+'.pdf',
+			image:        { type: 'jpeg', quality: 0.98 },
+			html2canvas:  { scale: 2 },
+			jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' },
+			pagebreak: { mode: 'avoid-all' }
+		};
+		html2pdf().set(opt).from(element).save();
+		///////
     }
 </script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.min.js"></script>
