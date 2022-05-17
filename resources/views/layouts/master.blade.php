8614f2227e6c28933f91729fc6f75227ad3ea893
rosaga <richardjohnzranger@gmail.com>
2022-05-17 12:21:36 +0300

    update home link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -90,7 +90,7 @@
 						
 					</ul>
 					<ul class="nav navbar-nav navbar-right">
-						<li><a href="https://indextestingapp.uonbi.ac.ke/api/apps/Partner-Reporting-Dashboards/html/index.html" >Home</a></li>
+						<li><a href="https://partnermanagementsystem.uonbi.ac.ke/api/apps/Partner-Reporting-Dashboards/html/index.html" >Home</a></li>
                         <li><a href="{{ url('/guide') }}">User Guide</a></li>
 						@guest
 							<li><a href="{{ url('/login') }}">Login</a></li>
