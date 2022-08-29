e67d42987da75e168bceecb2b9bbfe21648f7d6c
injiri <simoninjiri@gmail.com>
2022-08-29 18:00:26 +0300

    auth redirection

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -13,6 +13,17 @@ use DB;
 class GeneralController extends Controller
 {
 
+	/**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
 	public function partner_home()
 	{
 		session(['financial' => false]);
